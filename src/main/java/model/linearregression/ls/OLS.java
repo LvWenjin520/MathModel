050c43a0a9729a9e5dccf29a12f74a75a9015f66
LvWenJin <972950811@qq.com>
2021-06-03 22:55:56 +0800

    推送

---
+++ src/main/java/model/linearregression/ls/OLS.java
@@ -0,0 +1,94 @@
+package model.linearregression.ls;
+
+import Jama.Matrix;
+
+/**
+ * 普通最小二乘法
+ * @author LvWenJin
+ *
+ */
+public class OLS {
+	
+	//直线拟合法
+	//最小二乘法的方法
+	public Matrix oLS(double[][] points) {
+		int row = points.length;
+		//两列
+		double[][] X = new double[row][2];
+		//一列
+		double[][] y = new double[row][1];
+		for(int i=0;i<row;i++) {
+			X[i][0] = 1;
+			X[i][1] = points[i][0];
+			y[i][0] = points[i][1];
+		}
+		//获取结果
+		return getSLResult(X,y);
+	}
+	
+	/**
+	 *    n次多项式曲线拟合
+	 * @param points
+	 * @param n 最高次幂,n要大于等于2
+	 * @param constant  是否需要常数项
+	 * @return
+	 */
+	public Matrix oLS(double[][] points,int n,boolean constant) {
+		if(n<2) {
+			return null;
+		}
+		int row = points.length;
+		//如果需要常数项
+		if(constant) {
+			//n+1列
+			double[][] X = new double[row][n+1];
+			//一列，y列
+			double[][] y = new double[row][1];
+			for(int i=0;i<row;i++) {
+				for(int j=0;j<=n;j++) {
+					X[i][j] = Math.pow(points[i][0], j);
+				}
+				y[i][0] = points[i][1];
+			}
+			//获取结果
+			return getSLResult(X,y);
+		}else {  //不需要常数项
+			//n列
+			double[][] X = new double[row][n];
+			//一列，y列
+			double[][] y = new double[row][1];
+			for(int i=0;i<row;i++) {
+				for(int j=0;j<n;j++) {
+					X[i][j] = Math.pow(points[i][0], j+1);
+				}
+				y[i][0] = points[i][1];
+			}
+			//获取结果
+			return getSLResult(X,y);
+		}
+	}
+	
+	/**
+	 * 求最小二乘解
+	 * @param X
+	 * @param y
+	 * @return  最终的系数
+	 */
+	private Matrix getSLResult(double[][] X,double[][] y) {
+		//矩阵X
+		Matrix matrixX = new Matrix(X);
+		//结果向量y
+		Matrix matrixY = new Matrix(y);
+		//x的置转矩阵
+		Matrix transposeX = matrixX.transpose();
+		//乘法之后的矩阵
+		Matrix XtX = transposeX.times(matrixX);
+		//置转矩阵乘以结果向量
+		Matrix Xty = transposeX.times(matrixY);
+		//求逆
+		Matrix inverse = XtX.inverse();
+		//第一行是常数项，第二行是一次方程系数
+		return inverse.times(Xty);
+	}
+	
+}
---
+++ src/test/java/testmodels/Test.java
@@ -0,0 +1,32 @@
+package testmodels;
+
+import Jama.Matrix;
+import model.linearregression.ls.OLS;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		OLS o = new OLS();
+		double[][] points = new double[5][2];
+		
+		double[] L1 = {1.0,1.8};
+		double[] L2 = {2.0,2.7};
+		double[] L3 = {3.0,3.4};
+		double[] L4 = {4.0,3.8};
+		double[] L5 = {5.0,3.9};
+		
+		points[0]=L1;
+		points[1]=L2;
+		points[2]=L3;
+		points[3]=L4;
+		points[4]=L5;
+		
+		Matrix oLS1 = o.oLS(points,3,false);
+		
+		for(int i = 0;i<oLS1.getRowDimension();i++) {
+			System.out.println(oLS1.get(i, 0));
+		}
+	}
+	
+}
